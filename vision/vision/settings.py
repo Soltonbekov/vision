56efcef234d61ec3984274fa55462f612d171a99
Soltonbekov <asoltonbekov@gmail.com>
2016-08-15 22:54:17 +0600

    Income app in progress

--- vision/vision/settings.py
+++ vision/vision/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'notification',
     'accounts',
     'profiles',
+    'income',
+    'fontawesome',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -125,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+FONTAWESOME_CSS_URL = '//cdn.example.com/fontawesome-min.css' # absolute url
+FONTAWESOME_CSS_URL = 'myapp/css/fontawesome.min.css' # relative url
